2ebe9058091bf1a4f6319caa14aa017f7b8ac37d
Jairo Reyes <ing.jairoreyes@gmail.com>
2021-09-22 11:11:53 -0400

    feature: fixing properties rename on getAllProjects

--- src/app/modules/admin/shared/services/project.service.ts
+++ src/app/modules/admin/shared/services/project.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, } from 'rxjs';
+import { map } from 'rxjs/Operators';
 import { Project } from '@admin/model/project';
 
 @Injectable()
@@ -11,6 +12,16 @@ export class ProjectService {
   constructor(private httpClient: HttpClient) { }
 
   getAllProjects(): Observable<Array<Project>> {
-    return this.httpClient.get<Array<Project>>(this.GET_PROJECT_URL);
+    return this.httpClient.get<Response>(this.GET_PROJECT_URL)
+                    .pipe(
+                      map((data: any) => {
+                        return data.map((item:any) => {
+                          item["id"] = item.projectID;
+                          item["name"] = item.projectName;
+                          item["startDate"] = item.dateOfStart;
+                          return item;
+                        })
+                      }
+                    ));
   }
 }
